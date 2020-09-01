b10494ae2cfbb359210c92b2f6701fc2410cb855
Srijan Singh <srijan.singh.1232@gmail.com>
2020-09-01 14:41:24 +0530

    done

--- component/Layout.js
+++ component/Layout.js
@@ -23,6 +23,17 @@ function Landing(props){
             <meta name="msapplication-TileColor" content="#ffffff" />
             <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
             <meta name="theme-color" content="#000000" />
+
+
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176938786-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-176938786-1');
+            </script>
+
         </Head>
         <div className="container">
             <Navbar />
