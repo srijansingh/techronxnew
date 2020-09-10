133fed5f09de9b682361328a0a1790af7c0c11d4
Srijan Singh <srijan.singh.1232@gmail.com>
2020-09-10 19:14:02 +0530

    video

--- component/tools/Modal.js
+++ component/tools/Modal.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+
 import Dialog from '@material-ui/core/Dialog';
-import Paper from '@material-ui/core/Paper';
-import Draggable from 'react-draggable';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
+
 
 export default function TransitionsModal(props) {
   const [open, setOpen] = React.useState(false);
@@ -13,27 +10,22 @@ export default function TransitionsModal(props) {
     setOpen(false);
   };
 
-  function PaperComponent(props) {
-    return (
-      <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
-        <Paper {...props} />
-      </Draggable>
-    );
-  }
+ 
 
+  
   return (
-    
+      
       <Dialog
      
-      style={{backgroundColor:'black'}}
+       
         open={props.isOpen}
         onClose={handleClose}
-        PaperComponent={PaperComponent}
+       
         aria-labelledby="draggable-dialog-title"
         aria-describedby="alert-dialog-description"
-        style={{outline:'none'}}
+       
       >
-          <div style={{ cursor: 'move' }} id="draggable-dialog-title">
+          <div>
               
               {props.children}
               </div>
